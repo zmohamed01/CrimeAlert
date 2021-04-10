79a59b5d91e4eeb036f47ed962f753ff8fee9e9d
zmohamed01 <72342841+zmohamed01@users.noreply.github.com>
2021-04-10 22:21:26 +0100

    Revert "Lapids branch"

--- db/migrate/20210410021856_add_datetime_to_locations.rb
+++
@@ -1,5 +0,0 @@
-class AddDatetimeToLocations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :locations, :datetime, :float
-  end
-end
--- db/migrate/20210410030212_add_image_to_locations.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToLocations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :locations, :image, :string
-  end
-end
