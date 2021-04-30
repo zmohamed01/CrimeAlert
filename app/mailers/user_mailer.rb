ac383332cde87b01fbfce7952b24a8c677cf5bbe
zmohamed01 <72342841+zmohamed01@users.noreply.github.com>
2021-04-30 12:46:10 +0100

    Implementation of mailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,13 @@ class UserMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.contact_form.subject
+  #   en.user_mailer.contact_form.subjecthow to present database rails boostrap
   #
-  def contact_form
-    @greeting = "Hi"
+  def contact_form(name, email, message)
+    @name = name
+    @email = email
+    @message = message
 
-    mail to: "to@example.org"
+    mail to: "crimeguardapp@gmail.com", subject: "You've got a new message!"
   end
 end
