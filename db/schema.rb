0f46cdcfe3f776ffdf75c9c72f8fe059985e641e
ht00524 <ht00524@surrey.ac.uk>
2021-05-15 14:40:40 +0100

    added user devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_025134) do
+ActiveRecord::Schema.define(version: 2021_05_14_153622) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,18 @@ ActiveRecord::Schema.define(version: 2021_05_10_025134) do
     t.text "datetime"
     t.string "image"
     t.string "city"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "places", force: :cascade do |t|
